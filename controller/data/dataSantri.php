69a887206c368f56caa5e3f4db3944a185d107dc
Jorlex27 <alexveros46@gmail.com>
2024-04-26 20:15:26 +0700

    coba class

---
+++ controller/data/dataSantri.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("../../config/connect.php");
+class DataSantri{
+    private $db;
+    public function __construct(){
+        $this->db = new dataBase;
+    }
+
+    public function getData(){
+        $query = "SELECT * FROM santri WHERE status = 'AKTIF'";
+        $result = $this->db->getAllRows($query);
+        return $result;
+    }
+}
+
+$c = new DataSantri;
+$data = $c->getData();
+// print_r($data);
+
+// $db = new dataBase;
+// $query = "SELECT * FROM santri WHERE status = 'AKTIF'";
+// $result = $db->getlimit($query, 1000, 10);
+
+// print_r($result);
+
+foreach($data as $row){
+    print_r($row);
+}
